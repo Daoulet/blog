bb30fd325e734109a309d93783b2c5126cbb58af
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2018-09-27 22:43:07 +0600

    nineth commit

--- mainApp/views.py
+++ mainApp/views.py
@@ -4,7 +4,7 @@ def index(request):
     return render (request, 'mainApp/homePage.html')
 
 def contact(request):
-    return render (request, 'mainApp/basic.html', {'values': ['Если у вас остались вопросы, то задавайте их мне по телефону', '8777546231221221']})
+    return render (request, 'mainApp/basic.html')
 
 def resume(request):
     return render (request, 'mainApp/resume.html')
