0a6b1ffcf9a7fb78f67930baa64890778b5860ec
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2019-02-01 22:24:57 +0600

    11 commit

--- mainApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render (request, 'mainApp/homePage.html')
-
-def resume(request):
-    return render (request, 'mainApp/resume.html')
