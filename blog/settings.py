834408dc97aada65d972e230d3b23d439c54b83f
Daoulet Bimaganbetov <bimaganbetov1995@gmail.com>
2019-02-10 12:01:55 +0600

    21 commit

--- blog/settings.py
+++ blog/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = BASE_DIR + 'static/media'
+
+MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
